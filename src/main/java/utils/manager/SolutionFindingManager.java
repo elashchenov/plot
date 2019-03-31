0e66d085a713e03313b3dc9113768eabfad05780
Evgeny Lashchenov <latshenov.ei@edu.spbstu.ru>
2019-03-31 17:51:57 +0300

    bug fixed

--- src/main/java/utils/manager/SolutionFindingManager.java
+++ src/main/java/utils/manager/SolutionFindingManager.java
@@ -63,18 +63,19 @@ public class SolutionFindingManager {
 
     /**
      * Вычисляет пересечени эллипса прямой(не отрезка!!!) от двух точек р1 и р2.
-     *      О принадлежности отрезка отрезку от двух точек вычисляется после использования этого метода(выше)
+     * О принадлежности отрезка отрезку от двух точек вычисляется после использования этого метода(выше)
+     *
      * @param p1 - первая точка
      * @param p2 - вторая точка
      * @return - пара точек - пересечение прямой из двух входных точках и эллипса.
-     *      Возвращает null, если двух точек найдено не было(нет пересечения)
+     * Возвращает null, если двух точек найдено не было(нет пересечения)
      */
     private static Pair<Point, Point> computeEllipseIntersect(Point p1, Point p2) {
         double radiusX = ellipse.getRadiusX();
         double radiusY = ellipse.getRadiusY();
         double centerX = ellipse.getCenterX();
         double centerY = ellipse.getCenterY();
-        double a, k, c; // Эти три числа - коэффициенты квадратичного уравнения, получаемого на основе решение системы:
+        double a, b, c; // Эти три числа - коэффициенты квадратичного уравнения, получаемого на основе решение системы:
         //первое уравнение - уравнение эллипса, второе - уравнение прямой, образованного от точек p1 и p2
         Point resPoint1 = null;
         Point resPoint2 = null;
@@ -84,9 +85,9 @@ public class SolutionFindingManager {
         if (p1.getX() == p2.getX()) { //если отрезок от двух точек параллелен оси Oy
             double d = Math.pow(p1.getX() - centerX, 2) * Math.pow(radiusY, 2) / Math.pow(radiusX, 2);
             a = 1;
-            k = -centerY;
+            b = -2 * centerY;
             c = Math.pow(centerY, 2) + d - Math.pow(radiusY, 2);
-            Pair<Double, Double> ySolution = solveQuadraticEquation(a, k, c);
+            Pair<Double, Double> ySolution = solveQuadraticEquation(a, b, c);
             if (ySolution.getKey() != null) {
                 resPoint1 = new Point(p1.getX(), ySolution.getKey());
                 resPoint2 = new Point(p1.getX(), ySolution.getValue());
@@ -95,24 +96,27 @@ public class SolutionFindingManager {
         } else if (p1.getY() == p2.getY()) {//если отрезок от двух точек параллелен оси Ox
             double d = Math.pow(p1.getY() - centerY, 2) * Math.pow(radiusX, 2) / Math.pow(radiusY, 2);
             a = 1;
-            k = -centerX;
+            b = -2 * centerX;
             c = Math.pow(centerX, 2) + d - Math.pow(radiusX, 2);
-            Pair<Double, Double> xSolution = solveQuadraticEquation(a, k, c);
+            Pair<Double, Double> xSolution = solveQuadraticEquation(a, b, c);
             if (xSolution.getKey() != null) {
                 resPoint1 = new Point(xSolution.getKey(), p1.getY());
                 resPoint2 = new Point(xSolution.getValue(), p1.getY());
                 success = true;
             }
         } else {//если отрезок от двух точек не параллелен осям
-            double b = (p2.getY() - p1.getY()) / (p2.getX() - p1.getX());
-            double d = p1.getY() - p1.getX() * b;
-            a = 1 / Math.pow(radiusX, 2) + Math.pow(b / radiusY, 2);
-            k = b * (d - centerY) / Math.pow(radiusY, 2) - centerX / Math.pow(radiusX, 2);
-            c = Math.pow(centerX * radiusX, 2) + Math.pow((d - centerY) / radiusY, 2) - 1;
-            Pair<Double, Double> xSolution = solveQuadraticEquation(a, k, c);
+            double k = (p2.getY() - p1.getY()) / (p2.getX() - p1.getX());
+            double l = (p1.getY() * p2.getX() - p1.getX() * p2.getY()) / (p2.getX() - p1.getX());
+            a = Math.pow(radiusY, 2) + Math.pow(radiusX * k, 2);
+            b = -2 * Math.pow(radiusY, 2) * centerX + 2 * Math.pow(radiusX, 2) * k * l
+                    - 2 * Math.pow(radiusX, 2) * k * centerY;
+            c = Math.pow(radiusY * centerX, 2) + Math.pow(radiusX * centerY, 2)
+                    - 2 * Math.pow(radiusX, 2) * l * centerY + Math.pow(radiusX * l, 2)
+                    - Math.pow(radiusX * radiusY, 2);
+            Pair<Double, Double> xSolution = solveQuadraticEquation(a, b, c);
             if (xSolution.getKey() != null) {
-                resPoint1 = new Point(xSolution.getKey(), xSolution.getKey() * b + d);
-                resPoint2 = new Point(xSolution.getValue(), xSolution.getValue() * b + d);
+                resPoint1 = new Point(xSolution.getKey(), xSolution.getKey() * k + l);
+                resPoint2 = new Point(xSolution.getValue(), xSolution.getValue() * k + l);
                 success = true;
             }
         }
@@ -122,16 +126,17 @@ public class SolutionFindingManager {
 
     /**
      * Решение квадратного уравние
+     *
      * @return - возвращает пару точек - решение. Возвращает null, если решение только одно
-     *      или не существует в области действительных чисел
+     * или не существует в области действительных чисел
      */
-    private static Pair<Double, Double> solveQuadraticEquation(double a, double k, double c) {
+    private static Pair<Double, Double> solveQuadraticEquation(double a, double b, double c) {
         Double x1 = null;
         Double x2 = null;
-        double discriminant = k * k - a * c;
+        double discriminant = b * b - 4 * a * c;
         if (discriminant > 0) {
-            x1 = (-k + Math.sqrt(discriminant)) / a;
-            x2 = (-k - Math.sqrt(discriminant)) / a;
+            x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
         }
 
         return new Pair<>(x1, x2);
@@ -139,6 +144,7 @@ public class SolutionFindingManager {
 
     /**
      * Определяет лежит ли точка в отрезке.
+     *
      * @param a - первая точка отрезка
      * @param b - вторая точка отрезка
      * @param p - проверяемая точка
